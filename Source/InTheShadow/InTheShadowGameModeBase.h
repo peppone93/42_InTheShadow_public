e6e9f400b9674ae3c9e3586f1317eed9dd6441c6
Giuseppe <giuseppebianco93@gmail.com>
2021-03-29 13:00:08 +0200

    sync

---
+++ Source/InTheShadow/InTheShadowGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InTheShadowGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTHESHADOW_API AInTheShadowGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
